164ccd91388403ced36d513c9b39ea517e5d7f4a
jonmarinello <jonmarinello@gmail.com>
2014-06-26 13:42:33 -0700

    Added the main navbar.

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,4 +1,21 @@
 class StaticsController < ApplicationController
   def index
+    @active_tab = 'index'
+  end
+
+  def projects
+    @active_tab = 'projects'
+  end
+
+  def about
+    @active_tab = 'about'
+  end
+
+  def services
+    @active_tab = 'services'
+  end
+
+  def contact
+    @active_tab = 'contact'
   end
 end
