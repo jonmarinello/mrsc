e2fb53b8607f4bafadd426e46d832edb037c69a1
Jon Marinello <jonmarinello@gmail.com>
2014-09-28 17:15:06 -0700

    Added projects page test.

--- test/Features/pages_test.rb
+++ test/Features/pages_test.rb
@@ -8,9 +8,24 @@ class PagesTest < Capybara::Rails::TestCase
     # Validate we landed on the correct page
     assert_on_page_path '/'
 
-    # Validate content
+    # Validate expected content
     must_have_content 'Mission Ridge Software Consulting LLC'
     must_have_content 'Welcome To Mission Ridge Software Consulting'
+    must_have_content 'Have a project we can help with?'
+    must_have_content 'Stay Connected With Us'
+
+    # Visit Projects tab
+    click_link('Projects', match: :first)
+
+    # Validate we landed on the correct page
+    assert_on_page_path '/pages/projects'
+
+    # Validate expected content
+    must_have_content 'Mission Ridge Software Consulting LLC'
+    must_have_content 'We work well with others'
+    must_have_content 'Fin & Field is all about the sportsman. Dedicated to those in search of their next outdoor adventure and to the outfitters, captains, and guides that make it all possible.'
+    must_have_content 'RightScale provides a complete management platform to design, deploy, and manage the lifecycle of mission-critical cloud deployments.'
+    must_have_content 'Elite Associates is involved in all aspects of the design, development and delivery of StarTeam to Fortune 1000 customers and uses that experience to help them get the most out of StarTeam.'
     must_have_content 'Have a project we can help with?'
     must_have_content 'Stay Connected With Us'
   end
