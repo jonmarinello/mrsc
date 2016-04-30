92500574c80d7b361b00072e42c9b894459de374
Jon Marinello <jonmarinello@gmail.com>
2016-04-30 09:23:58 -0700

    Backed out browser caching.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable browser caching (1 week)
-  config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=#{1.week.to_i}"
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
